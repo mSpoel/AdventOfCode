46cae2d9986f3e96cbcf4af07751703e5c57db0a
Mark van der Spoel <m.vanderspoel@gmail.com>
2023-12-11 09:18:11 +0100

    Add skeleton day 11

---
+++ Solutions/2023/Day11/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+var watch = Stopwatch.StartNew();
+
+var solver = new SolverPart1();
+
+//Console.WriteLine($"Solution: {solver.GetSolution("example02.txt")}");
+Console.WriteLine($"Solution: {solver.GetSolution("input.txt")}");
+Console.WriteLine($"Elapsed: {watch.ElapsedMilliseconds}ms");
---
+++ Solutions/2023/Day11/SolverPart1.cs
@@ -0,0 +1,17 @@
+﻿
+internal class SolverPart1
+{
+    internal int GetSolution(string inputPath)
+    {
+        string[] lines = File.ReadAllLines(inputPath);
+
+        int result = 0;
+
+        foreach (var line in lines)
+        {
+
+        }
+
+        return result;
+    }
+}
