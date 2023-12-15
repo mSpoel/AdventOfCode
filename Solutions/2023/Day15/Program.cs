7ac4d4c0aca1c8cc208db4e161fcdbe6f8b6dcf7
Mark van der Spoel <m.vanderspoel@gmail.com>
2023-12-15 07:00:50 +0100

    Add day15 skeleton

---
+++ Solutions/2023/Day15/Program.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics;
+using Day15;
+
+var watch = Stopwatch.StartNew();
+
+var solver = new SolverPart1();
+
+Console.WriteLine($"Solution: {solver.GetSolution("example.txt")}");
+//Console.WriteLine($"Solution: {solver.GetSolution("input.txt")}");
+Console.WriteLine($"Elapsed: {watch.ElapsedMilliseconds}ms");
