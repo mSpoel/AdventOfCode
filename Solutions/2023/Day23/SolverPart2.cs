5b938e810ba0b5c8b59c1b82b30d67a0ee062b78
Mark van der Spoel <m.vanderspoel@gmail.com>
2023-12-28 10:16:18 +0100

    Add solution for part2 day23

--- Solutions/2023/Day23/SolverPart2.cs
+++ Solutions/2023/Day23/SolverPart2.cs
@@ -4,7 +4,7 @@ namespace Day23
 {
     internal class SolverPart2
     {
-        internal int GetSolution(string inputPath)
+        internal long GetSolution(string inputPath)
         {
             string[] lines = File.ReadAllLines(inputPath);
 
@@ -19,48 +19,148 @@ namespace Day23
             var start = (0, 1);
             var end = (grid.NumberOfRows - 1, grid.NumberOfColumns - 2);
 
-            var stack = new Stack<((int row, int column) coordinate, HashSet<(int row, int column)> path)>();
-            HashSet<HashSet<(int row, int column)>> paths = new HashSet<HashSet<(int row, int column)>>();
-            stack.Push((start, new HashSet<(int row, int column)>()));
+            var intersections = GetIntersections(grid);
+            intersections.Add(start);
+            intersections.Add(end);
 
-            Dictionary<(int row, int column), int> memo = [];
-            var result = 0;
-            var stackSize = 0;
+            Dictionary<(int row, int column), Dictionary<(int row, int column), int>> distances = [];
+            foreach (var intersection in intersections)
+            {
+                var distance = BreadthFirstSearch(grid, intersection, intersections);
+                distances.Add(intersection, distance);
+                Console.Write($"{intersection}: ");
+                foreach (var d in distance)
+                {
+                    Console.Write($" {d.Key}: {d.Value}");
+                }
+                Console.WriteLine();
+
+            }
+
+            return DepthFirstSearch(distances, start, end);
+        }
+
+        private static long DepthFirstSearch(Dictionary<(int row, int column), Dictionary<(int row, int column), int>> graph, (int row, int column) start, (int row, int column) end)
+        {
+            long maxDistance = 0;
+            var stack = new Stack<((int row, int column) coordinate, int distance, HashSet<(int row, int column)> visited)>();
+            var initVisit = new HashSet<(int row, int column)>
+            {
+                start
+            };
+            stack.Push((start, 0, initVisit));
 
             while (stack.Count > 0)
             {
-                var (coordinate, path) = stack.Pop();
+                var (coordinate, distance, visited) = stack.Pop();
+
+                //Console.WriteLine($"Coordinate: {coordinate} \t Distance: {distance} \t Visited: {visited.Count}");
 
                 if (coordinate == end)
                 {
-                    if (!paths.Contains(path))
-                    {
-                        result = Math.Max(result, path.Count);
-                        paths.Add(path);
-                        Console.WriteLine($"Path found with length: {path.Count}. Result so far {result}");
-                    }
+                    maxDistance = Math.Max(maxDistance, distance);
+                    Console.WriteLine($"Found path with distance: {distance}. Max distance {maxDistance}");
                     continue;
                 }
-                foreach (var direction in Enum.GetValues<Direction>())
+
+                foreach (var neighbor in graph[coordinate])
                 {
-                    var next = grid.GetNextPoint(coordinate, direction);
-                    if (!grid.IsInGrid(next) || grid.Get(next) == '#' || path.Contains(next))
+                    if (visited.Contains(neighbor.Key))
                     {
                         continue;
                     }
-                    var updatedPath = new HashSet<(int row, int column)>(path);
-                    updatedPath.Add(next);
-                    stack.Push((next, updatedPath));
-                }
 
-                if (stack.Count != stackSize)
-                {
-                    stackSize = stack.Count;
-                    Console.WriteLine($"Stack size: {stack.Count}");
+                    var newDistance = distance + neighbor.Value;
+                    var newVisited = new HashSet<(int row, int column)>(visited)
+                    {
+                        neighbor.Key
+                    };
+                    stack.Push((neighbor.Key, newDistance, newVisited));
+
+                    //Console.Write($"Neighbor: {neighbor.Key} \t Distance: {neighbor.Value} \t newDistance {newDistance}");
+                    //foreach (var v in newVisited)
+                    //{
+                    //    Console.Write($" {v}");
+                    //}
+                    //Console.WriteLine();
                 }
             }
 
-            return paths.Max(p => p.Count);
+            return maxDistance;
+        }
+
+        private static Dictionary<(int row, int column), int> BreadthFirstSearch(Grid grid, (int row, int column) start, List<(int row, int column)> intersections)
+        {
+            var queue = new Queue<((int row, int column), int distance)>();
+            var visited = new HashSet<(int row, int column)>();
+            var distances = new Dictionary<(int row, int column), int>();
+
+            queue.Enqueue((start, 0));
+
+            while (queue.Count > 0)
+            {
+                var (coordinate, distance) = queue.Dequeue();
+
+                if (intersections.Contains(coordinate) && coordinate != start)
+                {
+                    distances.Add(coordinate, distance);
+                    continue;
+                }
+
+                var nextDistance = distance + 1;
+                foreach (var direction in Enum.GetValues<Direction>())
+                {
+                    var next = grid.GetNextPoint(coordinate, direction);
+
+                    if (!grid.IsInGrid(next) || grid.Get(next) == '#' || visited.Contains(next))
+                    {
+                        continue;
+                    }
+
+                    visited.Add(next);
+                    queue.Enqueue((next, nextDistance));
+                }
+            }
+
+            return distances;
+        }
+
+        private static List<(int, int)> GetIntersections(Grid grid)
+        {
+            // Find all tiles that have more than 2 neighbors
+            var intersections = new List<(int, int)>();
+
+            for (int row = 0; row < grid.NumberOfRows - 1; row++)
+            {
+                for (int col = 0; col < grid.NumberOfColumns - 1; col++)
+                {
+                    if (grid.Get((row, col)) == '#')
+                    {
+                        continue;
+                    }
+
+                    int neighbors = 0;
+
+                    foreach (var direction in Enum.GetValues<Direction>())
+                    {
+                        var next = grid.GetNextPoint((row, col), direction);
+
+                        if (!grid.IsInGrid(next) || grid.Get(next) == '#')
+                        {
+                            continue;
+                        }
+
+                        neighbors++;
+                    }
+
+                    if (neighbors > 2)
+                    {
+                        intersections.Add((row, col));
+                    }
+                }
+            }
+
+            return intersections;
         }
     }
 }
