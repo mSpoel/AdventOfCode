d94f11eed235928aa246d56b01cf018dfc1893ec
Mark van der Spoel <m.vanderspoel@gmail.com>
2023-12-30 07:57:49 +0100

    Use char.IsUpper instead of own implementation

--- Solutions/2022/Day03/CharCalculator.cs
+++ Solutions/2022/Day03/CharCalculator.cs
@@ -4,13 +4,11 @@
     {
         internal static int GetValue(char c)
         {
-#pragma warning disable CA1862 // Use the 'StringComparison' method overloads to perform case-insensitive string comparisons
-            if (c.ToString().ToUpper() == c.ToString())
+            if (char.IsUpper(c))
             {
                 // Ascii A = 65 -> should map to 27
                 return c - (65 - 27);
             }
-#pragma warning restore CA1862 // Use the 'StringComparison' method overloads to perform case-insensitive string comparisons
 
             // Ascii a = 97 -> should map to 1
             return c - (97 - 1);
